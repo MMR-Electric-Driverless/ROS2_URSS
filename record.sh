c34a5938b51e89e36621fe3884a76380086ee95c
alexmara <email>
2026-02-18 22:10:20 +0100

    add pcap.log and bag.log

--- record.sh
+++ record.sh
@@ -63,13 +63,13 @@ pid_pcap=""
 
 # tcpdump
 if [ "$pcap" -eq 0 ]; then 
-    tcpdump $pcap_args & 
+    tcpdump $pcap_args &> pcap.log &
     pid_pcap=$! 
     echo "pid_pcap=$pid_pcap" 
 fi
 # ros2 bag record $bag_args --topics:$topics
 if [ "$bag" -eq 0 ]; then 
-    ros2 bag record $bag_args $topics & 
+    ros2 bag record $bag_args $topics &> bag.log &
     pid_bag=$! 
     echo "pid_bag=$pid_bag" 
 fi
