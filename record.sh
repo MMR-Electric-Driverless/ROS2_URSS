bcabea6ba944a616246a518807502e4ca1877b6b
diego-graziati <dgraziati@gmail.com>
2026-03-10 21:46:12 +0100

    Fixed error with merge.
    Tested ros2 bag record and timestamp feature.

--- record.sh
+++ record.sh
@@ -9,6 +9,10 @@ bag_val=$(yaml-parser ${preset_filename} "bag" | sed 's/\x1b[[0-9;]*m//g')
 bag_args_val=$(yaml-parser ${preset_filename} "bag_args" | sed 's/\x1b[[0-9;]*m//g')
 topics_val=$(yaml-parser ${preset_filename} "topics" | sed 's/\x1b[[0-9;]*m//g')
 pcap_args_val=$(yaml-parser ${preset_filename} "pcap_args" | sed 's/\x1b[[0-9;]*m//g')
+pcap_ofile_name_raw=$(yaml-parser ${preset_filename} pcap_name | sed 's/\x1b[[0-9;]*m//g')
+bag_ofile_name_raw=$(yaml-parser ${preset_filename} bag_name | sed 's/\x1b[[0-9;]*m//g')
+
+date_format=$(yaml-parser ${preset_filename} date_format | sed 's/\x1b[[0-9;]*m//g')
 
 pcap=1
 echo "$pcap_val" = "true"
@@ -23,21 +27,21 @@ if [ "$bag_val" = "true" ]; then
 fi
 
 ## DEBUG
-echo \""$pcap_val"\" $pcap
-echo \""$bag_val"\" $bag
-echo \""$bag_args_val"\"
-echo \""$topics_val"\"
-echo \""$pcap_args_val"\"
+# echo \""$pcap_val"\" $pcap
+# echo \""$bag_val"\" $bag
+# echo \""$bag_args_val"\"
+# echo \""$topics_val"\"
+# echo \""$pcap_args_val"\"
 
-bag_args=$bag_args_val
+# bag_args=$bag_args_val
 
-pcap_args=$pcap_args_val
+# pcap_args=$pcap_args_val
 
-topics=$topics_val
+# topics=$topics_val
 
 
 
-## PROCCESSING OUTPUT FILE NAMES
+## PROCESSING OUTPUT FILE NAMES
 timestamp=$(date -d "today" +"$date_format")
 
 
@@ -45,7 +49,7 @@ if [ "$pcap_ofile_name_raw" = "null" ]; then
     pcap_ofile_name_arg=""
 else
     pcap_ofile_name=${pcap_ofile_name_raw/TIMESTAMP/$timestamp}
-    pcap_ofile_name_arg="-w ${pcap_ofile_name}"
+    pcap_ofile_name_arg="-w ""$pcap_ofile_name"
 fi
 
 
@@ -53,7 +57,7 @@ if [  "$bag_ofile_name_raw" = "null" ]; then
     bag_ofile_name_arg=""
 else
     bag_ofile_name=${bag_ofile_name_raw/TIMESTAMP/$timestamp}
-    bag_ofile_name_arg="-o "${bag_ofile_name}
+    bag_ofile_name_arg="-o ""$bag_ofile_name"
 fi
 
 ## ROSBAG AND TCPDUMP PID
@@ -63,13 +67,13 @@ pid_pcap=""
 if [ "$pcap" -eq 0 ]; then 
     tcpdump $pcap_args $pcap_ofile_name_arg > pcap.log 2>&1 &
     pid_pcap=$! 
-    echo "pid_pcap=$pid_pcap" 
+    echo "pid_pcap=""$pid_pcap" 
 fi
 
 if [ "$bag" -eq 0 ]; then 
     ros2 bag record $bag_args $topics $bag_ofile_name_arg > bag.log 2>&1 &
     pid_bag=$! 
-    echo "pid_bag=$pid_bag" 
+    echo "pid_bag=""$pid_bag" 
 fi
 
 ## ROSBAG AND TCPDUMP PROCESSES KILLS
